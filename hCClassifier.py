f197038b5fdea2bf341a23cf827f2d5e8d8fba75
DanielRMiller <d.miller1.work@gmail.com>
2016-01-19 22:51:42 -0700

    Made hc classifier always return an appropriate result

--- hCClassifier.py
+++ hCClassifier.py
@@ -1,13 +1,19 @@
 # Create a class for a "HardCoded" classifier
 class HCClassifier:
 	"""A class that is hardcoded to give a specific answer no matter the data"""
+	def __init__(self):
+		self.pre = "";
 	# Train: Accepts a training instances and targets. Does nothing with them.
-	def train(self, instances, targets): return
+	def train(self, instances, targets):
+		self.pre = targets[0]
+		print (self.pre)
+		return
 	# Predict: Accepts the test instances and returns a hard-coded response.
 	def predict(self, instances):
 		predictions = [];
 		for instance in instances:
-			predictions.append(0)
+			predictions.append(self.pre)
 		return predictions;
 	def fit(self, data, targets):
 	    self.train(data, targets)
+
